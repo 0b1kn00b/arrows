2ddc2c6d15959abc9c610a2c3e730bcdaa3151e0
laurence taylor <polysemantic@gmail.com>
2011-09-13 14:40:49 +0200

    functioning but Types giving problem

---
+++ unit/arrow/verb/DirectionTest.hx
@@ -0,0 +1,28 @@
+package arrow.verb;
+
+/**
+ * ...
+ * @author 0b1kn00b
+ */
+import haxe.test.TestCase;
+import haxe.test.Assert;
+
+import Prelude;
+
+import arrow.Viaz;
+using Viaz.Viaz;
+
+class DirectionTest extends TestCase{
+
+	public function new() {
+		super();
+	}
+	public function testLeft() {
+		var f = function(x:Bool) { return switch(x){ case true : Either.Right("yeah") ;  case false : Either.Left("nergen"); }} .lift();
+		f.then( A.i().print().left() ).run(true).start();
+	}
+	public function testRight() {
+		var f = function(x:Bool) { return switch(x){ case true : Either.Right("yeah") ;  case false : Either.Left("nergen"); }} .lift();
+		f.then( A.i().print().right() ).run(true).start();
+	}
+}
